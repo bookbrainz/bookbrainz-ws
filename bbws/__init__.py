7adcc7402354f4a88e4d021c5f4044c4364fe6df
Ben Ockmore <ben.sput@gmail.com>
2015-01-09 18:37:57 +0000

    Cleaned up __init__.py, added docstrings and header.

--- bbws/__init__.py
+++ bbws/__init__.py
@@ -1,9 +1,34 @@
+# -*- coding: utf8 -*-
+
+# Copyright (C) 2014-2015  Ben Ockmore
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
+""" This module stores a few program wide variables, and provides functions for
+creating an instance of the webservice application and adding CORS headers to
+webservice responses.
+"""
+
 
 from flask import Flask
-from flask.ext import restful
+from flask.ext.restful import Api
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask_redis import Redis
-from flask_oauthlib.provider import OAuth2Provider
+from flask.ext.redis import Redis
+from flask.ext.oauthlib.provider import OAuth2Provider
+
 
 db = SQLAlchemy()
 cache = Redis()
@@ -11,7 +36,11 @@ oauth_provider = OAuth2Provider()
 
 
 def add_cors_header(response):
-    # https://github.com/jfinkels/flask-restless/issues/223
+    """ Adds CORS headers to responses, so that cross-domain requests are
+    responded to successfully - see
+    https://github.com/jfinkels/flask-restless/issues/223
+    """
+
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = \
         'HEAD, GET, POST, PATCH, PUT, OPTIONS, DELETE'
@@ -23,30 +52,34 @@ def add_cors_header(response):
 
 
 def create_app(config_file):
+    """ Create the webservice application using the configuration provided in
+    config_file, and initialize SQLAlchemy, Redis and OAuth services. Also
+    installs webservice routes.
+    """
+
     app = Flask(__name__.split('.')[0])
     app.config.from_pyfile(config_file)
     app.after_request(add_cors_header)
 
-    api = restful.Api(app)
+    # Initialize Flask extensions
+    api = Api(app)
     db.init_app(app)
     cache.init_app(app)
     oauth_provider.init_app(app)
 
-    import test
-    import oauth as oauth_routes
-    oauth_routes.init(app)
-    test.init(app)
+    # Initialize OAuth handler
+    import bbws.oauth
+    bbws.oauth.init(app)
 
+    # Initialize webservice routes
     import bbws.entity
-    bbws.entity.create_views(api)
-
     import bbws.revision
-    bbws.revision.create_views(api)
-
     import bbws.user
-    bbws.user.create_views(api)
-
     import bbws.entityspecific
+
+    bbws.entity.create_views(api)
+    bbws.revision.create_views(api)
+    bbws.user.create_views(api)
     bbws.entityspecific.create_views(api)
 
     return app
