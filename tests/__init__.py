b8a3ddfed26774a9ced3e1f2b18303ff3841a9e2
stasszczesniak <stas.szczesniak@gmail.com>
2015-12-30 23:56:19 +0100

    Added TestCreator class for testing Creator GET, GET-list, PUT, POST and DELETE

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,2 +1,3 @@
 from test_user import *
 from test_relationship import *
+from test_creator import *
---
+++ tests/test_creator.py
@@ -0,0 +1,61 @@
+# -*- coding: utf8 -*-
+
+# Copyright (C) 2015 Stanisław Szcześniak
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from bbschema import Creator
+from entity_testing import EntityTestCase
+import json
+
+
+class TestCreator(EntityTestCase):
+
+    def specific_setup(self):
+        self.set_entity_class(Creator)
+        self.set_type_entity_name(u'Creator')
+        self.set_ws_entity_name('creator')
+
+        # There should be no aliases, disambiguations with the same name/sort_name in these files
+        with open('tests/samples/creator_1.json', 'r') as file_with_sample:
+            data = json.loads(file_with_sample.read())
+        self.put_post_data = data
+
+        with open('tests/samples/creator_1_put.json', 'r') as file_with_sample:
+            data = json.loads(file_with_sample.read())
+        self.put_only_data = data
+
+    def test_get_bbid(self):
+        self.bbid_get_tests()
+
+    def test_get_list(self):
+        self.list_get_tests()
+
+    def test_post(self):
+        self.post_tests()
+
+    def test_put(self):
+        self.put_tests()
+
+    def test_delete(self):
+        self.delete_tests()
+
+    def bbid_one_get_test_specific(self, instance, response, entity_gid):
+        pass
+
+
+
+
+
